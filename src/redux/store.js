690dc000db5bacb15de96330fc90e8b0e6813e33
Lait36 <118002242+Lait36@users.noreply.github.com>
2024-11-07 19:18:06 +0200

    deleted redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
 import {
-  persistStore,
-  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
@@ -11,19 +9,10 @@ import {
   PURGE,
   REGISTER,
 } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
-const persistConfig = {
-  key: "contacts", // ключ для збереження стану "contacts" 
-  storage,
-  whitelist: ["items"], // зберігання лише масиву контактів
-};
-
-const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: persistedContactsReducer,
+    contacts: contactsReducer,
     filter: filtersReducer,
   },
   middleware: (getDefaultMiddleware) =>
@@ -33,5 +22,3 @@ export const store = configureStore({
       },
     }),
 });
-
-export const persistor = persistStore(store);
