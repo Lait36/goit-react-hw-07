71e9bbf235d1fec8e5a89e78a8de42564eddcc89
Lait36 <118002242+Lait36@users.noreply.github.com>
2024-11-07 19:20:50 +0200

    deleted redux-persist 2.0

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
+
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filter: filtersReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
